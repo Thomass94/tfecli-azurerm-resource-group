e855ea870883690b1f9005d69764a17ac62a7f46
Alex Basista <alex.basista@gmail.com>
2019-09-18 16:37:36 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+  type        = string
+  description = "Azure Resource Group name"
+}
+
+variable "rg_location" {
+  type        = string
+  description = "Azure Resource Group region"
+}
